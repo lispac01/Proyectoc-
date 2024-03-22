74ea4bd48a8c29f9042e718424673da9e11a277f
Lisandro Pacheco <lisandrojpm@gmail.com>
2024-03-22 11:25:07 -0500

    cambios en la rama de desarrollo

---
+++ Deportista.cs
@@ -0,0 +1,19 @@
+using System;
+using static System.Console;
+namespace Proyectoc
+{
+public class Deportista : Persona
+{
+    public string? posicion { get; set; }
+
+    
+
+
+public void entrenar()
+{
+    WriteLine("Entrenando");
+}
+
+}
+
+}
