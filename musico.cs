1aed4d8a82d6293f10cbd59aebec84797cc06097
Lisandro Pacheco <lisandrojpm@gmail.com>
2024-03-22 15:17:23 -0500

    Se crea la clase musico

---
+++ musico.cs
@@ -0,0 +1,14 @@
+namespace Proyectoc;
+
+public class musico : Persona
+{
+ 
+ public string? instrumento_toca { get; set; }
+
+ public void IrConcierto()
+    {
+        Console.WriteLine("El musico va al concierto");
+    }
+
+    
+}
